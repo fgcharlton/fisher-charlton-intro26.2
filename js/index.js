28fe73e7a22ad6f7538bb2503cb3906470c8eaf4
fgcharlton <fgcharlton1@gmail.com>
2026-06-04 15:50:34 -0400

    added open api page

--- js/index.js
+++ js/index.js
@@ -96,7 +96,52 @@ fetch("https://api.github.com/users/fgcharlton/repos")
     )
     .catch(error => console.log('Unable to load projects', error))
 
+//Adding in temperature API
+fetch('https://api.open-meteo.com/v1/forecast?latitude=36.0726&longitude=-79.792&daily=temperature_2m_max,temperature_2m_min&timezone=America%2FNew_York&forecast_days=1&temperature_unit=fahrenheit&precipitation_unit=inch')
+    .then(response => response.json())
+    .then(temperature => {
+        console.log(temperature);
+        
+        //Create temperatureSection variable
+        const temperatureButton = document.createElement('button');
+
+        temperatureButton.textContent="What's the temperature?";
+        temperatureButton.className='temperatureButton';
+        temperatureButton.type='button';
+
+        temperatureButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+
+        if(entry) {
+            temperatureButton.textContent = `High of ${temperature.daily.temperature_2m_max}${temperature.daily_units.temperature_2m_max}. 
+            Low of ${temperature.daily.temperature_2m_min}${temperature.daily_units.temperature_2m_min}.`;
+        }
+        });
+        Weather.appendChild(temperatureButton);
+    })
+    .catch(error => console.log('Unable to load temperature Open API', error))
 
 
+//Adding in precipitation API
+fetch('https://api.open-meteo.com/v1/forecast?latitude=36.0726&longitude=-79.792&daily=precipitation_probability_max,precipitation_hours&timezone=America%2FNew_York&forecast_days=1&temperature_unit=fahrenheit&precipitation_unit=inch')
+    .then(response => response.json())
+    .then(precipitation => {
+        console.log(precipitation);
+        
+        //Create precipitationSection variable
+        const precipitationButton = document.createElement('button');
 
+        precipitationButton.textContent="What's the chance of rain?";
+        precipitationButton.className='precipitationButton';
+        precipitationButton.type='button';
 
+        precipitationButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+
+        if(entry) {
+        precipitationButton.textContent = `Today's chance of precipitation is ${precipitation.daily.precipitation_probability_max}${precipitation.daily_units.precipitation_probability_max}.`;
+        }
+        });
+        Weather.appendChild(precipitationButton);
+    })
+    .catch(error => console.log('Unable to load precipitation Open API', error))
