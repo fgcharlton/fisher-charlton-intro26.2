7b57de31335f0edacefeaeb1998802d68cf3f10d
fgcharlton <fgcharlton1@gmail.com>
2026-06-11 19:41:00 -0400

    added separate files for Open API as well as some minor revisions to index files

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ fetch("https://api.github.com/users/fgcharlton/repos")
         //Loop repositories
         for(let i = 0; i < repositories.length; i ++){
             let project = document.createElement('li');
-            project.innerHTML = `<a href="https://github.com/fgcharlton/${repositories[i].name}">${repositories[i].name}</a>`;
+            project.innerHTML = `<a href="https://github.com/fgcharlton/${repositories[i].name}" target="_blank" rel="noreferrer">${repositories[i].name}</a>`;
             
             //Append to project list 
             projectList.appendChild(project);
@@ -95,56 +95,3 @@ fetch("https://api.github.com/users/fgcharlton/repos")
         }
     )
     .catch(error => console.log('Unable to load projects', error))
-
-//Adding in temperature API
-fetch('https://api.open-meteo.com/v1/forecast?latitude=36.0726&longitude=-79.792&daily=temperature_2m_max,temperature_2m_min&timezone=America%2FNew_York&forecast_days=1&temperature_unit=fahrenheit&precipitation_unit=inch')
-    .then(response => response.json())
-    .then(temperature => {
-        console.log(temperature);
-        
-        //Create temperatureButton
-        const weatherSection = document.querySelector('#Weather');
-
-        const temperatureButton = document.createElement('button');
-
-        temperatureButton.textContent="What's the temperature?";
-        temperatureButton.className='temperatureButton';
-        temperatureButton.type='button';
-
-        temperatureButton.addEventListener("click", (event) => {
-        const entry = event.target.parentNode;
-
-        if(entry) {
-            temperatureButton.textContent = `High of ${temperature.daily.temperature_2m_max}${temperature.daily_units.temperature_2m_max}. 
-            Low of ${temperature.daily.temperature_2m_min}${temperature.daily_units.temperature_2m_min}.`;
-        }
-        });
-        weatherSection.appendChild(temperatureButton);
-    })
-    .catch(error => console.log('Unable to load temperature Open API', error))
-
-
-//Adding in precipitation API
-fetch('https://api.open-meteo.com/v1/forecast?latitude=36.0726&longitude=-79.792&daily=precipitation_probability_max,precipitation_hours&timezone=America%2FNew_York&forecast_days=1&temperature_unit=fahrenheit&precipitation_unit=inch')
-    .then(response => response.json())
-    .then(precipitation => {
-        console.log(precipitation);
-        
-        //Create precipitation button
-        const weatherSection = document.querySelector('#Weather');
-        const precipitationButton = document.createElement('button');
-
-        precipitationButton.textContent="What's the chance of rain?";
-        precipitationButton.className='precipitationButton';
-        precipitationButton.type='button';
-
-        precipitationButton.addEventListener("click", (event) => {
-        const entry = event.target.parentNode;
-
-        if(entry) {
-        precipitationButton.textContent = `Precipitation chance is ${precipitation.daily.precipitation_probability_max}${precipitation.daily_units.precipitation_probability_max}.`;
-        }
-        });
-        weatherSection.appendChild(precipitationButton);
-    })
-    .catch(error => console.log('Unable to load precipitation Open API', error))
